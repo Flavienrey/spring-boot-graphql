09744bdb952c715114b76a74ab28c42fda1cf329
Flavien Rey <wernete49@gmail.com~>
2024-08-20 12:50:00 +0200

    Added Unit test to verify queries + resources for queries

---
+++ src/test/java/com/flavien/GraphQLApplicationTests copy.java
@@ -0,0 +1,19 @@
+package com.flavien;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.graphql.GraphQlTest;
+import org.springframework.graphql.test.tester.GraphQlTester;
+
+@GraphQlTest(BookController.class)
+class GraphQLBookControllerTests {
+
+    @Autowired
+    private GraphQlTester graphQlTester;
+
+    @Test
+    void canGetBook() {
+        graphQlTester.documentName("books").execute().path("books").entityList(Book.class).hasSize(4);
+    }
+
+}
